927eb2d789a4bae73b41f0519e604730da148bcf
AniaCuapio <ania.cuapio@gmail.com>
2020-07-23 16:49:47 -0500

    algunas correcciones

--- src/models/entries.js
+++ src/models/entries.js
@@ -1,48 +1,41 @@
-
-
-
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const entriesSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 2,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  writer: {
+    type: String,
+    required: true,
+  },
+  imageurl: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    require: true,
+    enum: [
+      "Momentum",
+      "Coranavirus",
+      "Gen",
+      "Elemental",
+      "OneZero",
+      "Forge",
+      "Human Parts",
+      "Mind Cafe",
+      "UX Collective",
+      "The Journal Blog",
+    ],
+  },
+  date: {
+    type: Date,
+  },
+});
 
-    title: {
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 20
-    },
-    content: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: String,
-        required: true
-    },
-    imageurl: {
-        type: String,
-        required: true
-    },
-    category: {
-        type: String,
-        require: true,
-        enum:[
-            'Momentum',
-            'Coranavirus',
-            'Gen',
-            'Elemental',
-            'OneZero',
-            'Forge',
-            'Human Parts',
-            'Mind Cafe',
-            'Ux Collective',
-            'The Journal Blog'
-        ]
-    },
-    date: {
-        type: Date
-    }
-
-})
-
-
-module.exports = mongoose.model('entries', entriesSchema)
+module.exports = mongoose.model("entries", entriesSchema);
